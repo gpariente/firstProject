56899073d9a5af191ab4455e2999f5dc3192cdbe
gpariente <guypar1997@gmail.com>
2023-01-04 19:38:12 +0200

    feat: user now  able to enter chat id to send message to

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// Sets my token and user's id.
+// Sets user's token. ( my bot )
 const TOKEN = "5834958234:AAGA9IbJQHmi4FqKW4R2jSY7_hpqfnvf56g";
-const CHAT_ID = "496192745";
 
 const sendMessage = async () => {
   const message = document.getElementById("message").value;
+  const CHAT_ID = document.getElementById("chatId").value;
 
   const response = await fetch(
     `https://api.telegram.org/bot${TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=${message}`
